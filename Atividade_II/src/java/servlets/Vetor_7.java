1c624fd8fb5fe22d8e8056c3807ffb6a47ccf772
Hector Roberto <>
2017-04-01 01:20:02 -0300

    Questão 7 concluída. Iniciando questão 10 de Vetores.

---
+++ Atividade_II/src/java/servlets/Vetor_7.java
@@ -0,0 +1,108 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Administrador
+ */
+@WebServlet(name = "Vetor_7", urlPatterns = {"/vetor_7"})
+public class Vetor_7 extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        int vetor[] = new int[10];
+        int somaAdjacente[] = new int[5];
+        int i, aux, soma = 0, idMaior = 0;
+        
+        for (i = 0; i < 10; i++){
+            vetor[i] = Integer.valueOf(request.getParameter("valor"+i));
+        }
+        
+        for (i = 0; i < 5; i++){
+            for (int y = 0; y < 10; y+=2)
+                if(soma < (vetor[y]+vetor[y+1])){
+                    soma = vetor[y]+vetor[y+1];
+                    idMaior = y;
+                }
+        }
+        
+        Arrays.sort(vetor);
+        
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet Vetor_7</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1> As posições "+(idMaior+1)+" e "+((idMaior+1)+1)+" tem a maior soma de "+soma+" </h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
