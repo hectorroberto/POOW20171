23395b0a55aafbecbe5da2f0f90d078af148df30
Hector Roberto <>
2017-04-10 00:17:12 -0300

    Treinando for each e relembrando algumas bobagens esquecidas.

---
+++ JavaSE/src/Ducks/Duck.java
@@ -0,0 +1,6 @@
+package Ducks;
+
+public interface Duck {
+	public void quack();
+	public void fly();
+}
---
+++ JavaSE/src/Ducks/Turkey.java
@@ -0,0 +1,6 @@
+package Ducks;
+
+public interface Turkey {
+	public void gobble();
+	public void fly();
+}
