f44511d2834e5cfa6172b17322f182544f33a409
Hector Roberto <hectorroberto.ti@gmail.com>
2017-04-27 21:29:17 -0500

    Adicionando Banco de Dados SQL e seu Diagrama e versionando a documentação.

--- SAM/web/Documentação/BD-SAM v1.1.sql
+++ SAM/web/Documentação/BD-SAM v1.1.sql
@@ -4,21 +4,9 @@ SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 
--- -----------------------------------------------------
--- Schema mydb
--- -----------------------------------------------------
 
--- -----------------------------------------------------
--- Schema mydb
--- -----------------------------------------------------
 CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET utf8 ;
--- -----------------------------------------------------
--- Schema sam
--- -----------------------------------------------------
 
--- -----------------------------------------------------
--- Schema sam
--- -----------------------------------------------------
 CREATE SCHEMA IF NOT EXISTS `sam` DEFAULT CHARACTER SET utf8 ;
 USE `mydb` ;
 
